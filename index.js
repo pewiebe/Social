0e918eae6c39434ba73d334a091858c891994099
Samuel <118542381+pewiebe@users.noreply.github.com>
2022-12-04 15:44:23 -0300

    commit

---
+++ index.js
@@ -0,0 +1,60 @@
+// SIDEBAR
+const menuItems = document.querySelectorAll('.menu-item');
+
+// MESSAGES
+const messagesNotification = document.querySelector('#messages-notification');
+const messages = document.querySelector('.messages');
+const message = messages.querySelectorAll('.message');
+const messageSearch = document.querySelector('#message-search');
+
+
+// ================== SIDEBAR ========================
+
+
+// remover ativador class de todos os itens de menu
+const changeActiveItem = () => {
+    menuItems.forEach(item => {
+        item.classList.remove('active');
+    })
+}
+
+menuItems.forEach(item => {
+    item.addEventListener('click', () => {
+        changeActiveItem();
+        item.classList.add('active');
+        if (item.id != 'notifications') {
+            document.querySelector('.notifications-popup').
+                style.display = 'none';
+        } else {
+            document.querySelector('.notifications-popup').
+                style.display = 'block';
+            document.querySelector('#notifications .notification-count').style.display = 'none';
+        }
+    })
+})
+
+// ================== MESSAGES ========================
+// searches chat
+const searchMessage = () => {
+    const val = messageSearch.value.toLowerCase();
+    message.forEach(chat => {
+        let name = chat.querySelectorAll('h5').textContent.toLowerCase();
+        if (name.indexOf(val) != -1) {
+            chat.style.display = 'flex';
+        } else {
+            chat.style.display = 'none';
+        }
+    })
+}
+
+// search chat
+messageSearch.addEventListener('keyup', searchMessage);
+
+// destacar cartão de mensagens quando o item de menu de mensagens é clicado
+messagesNotification.addEventListener('click', () => {
+    messages.style.boxShadow = '0 0 1rem var(--color-primary)';
+    messagesNotification.querySelector('.notification-count').style.display = 'none';
+    setTimeout(() => {
+        messages.style.boxShadow = 'none';
+    }, 2000);
+})
